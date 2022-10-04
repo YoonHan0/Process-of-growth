566259d65028ce4d4882bcf5449a5f5fc2ad432d
HanYoung Yoon <87405950+YoonHan0@users.noreply.github.com>
2022-10-04 17:44:38 +0900

    DOM을 이용하여 HTML 값 객체에 저장

--- 4-nodejs를공부해보자/login-lecture/app/src/public/js/home/login.js
+++ 4-nodejs를공부해보자/login-lecture/app/src/public/js/home/login.js
@@ -1,4 +1,15 @@
 "use strict";
 
-console.log("Hello!");
-console.log("bye!!");
+const id = document.querySelector("#id");   // id 불러오기
+const psword = document.querySelector("#psword");   // id 불러오기
+const loginBtn = document.querySelector("button");  // button tag 불러오기
+
+loginBtn.addEventListener('click', login);
+
+function login() {
+    const req = {       // 서버로 넘겨주기 위해서 객체 형태로 id, pw 값을 저장함
+        id: id.value,
+        psword: psword.value,
+    };
+    console.log(req);
+}
