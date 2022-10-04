0cbd6ba94c1038ef39d580d5c6efe61ea4f81865
HanYoung Yoon <87405950+YoonHan0@users.noreply.github.com>
2022-10-04 18:00:48 +0900

    로그인을 요청하는 fetch 생성

--- 4-nodejs를공부해보자/login-lecture/app/src/public/js/home/login.js
+++ 4-nodejs를공부해보자/login-lecture/app/src/public/js/home/login.js
@@ -7,9 +7,16 @@ const loginBtn = document.querySelector("button");  // button tag 불러오기
 loginBtn.addEventListener('click', login);
 
 function login() {
-    const req = {       // 서버로 넘겨주기 위해서 객체 형태로 id, pw 값을 저장함
+    const req = {       // 서버로 넘겨주기 위해서 Object 형태로 id, pw 값을 저장함
         id: id.value,
         psword: psword.value,
     };
-    console.log(req);
+    // console.log(req, JSON.stringify(req));  // JSON.stringify(req): JSON 객체를 string형태로 변경해주는 작업, 출력하면 문자열로 나옴
+    fetch("/login", {       // fetch: Front에서 서버로 전달하는 작업, /login 경로에 해당하는 API가 만들어져 있어야 함(원래는)
+        method: "POST",
+        headers: {      // 전달하는 데이터가 JSON이라는 것을 알려줘야함
+            "Content-Type": "application/json", // JSON이라고 알려줌
+        },
+        body: JSON.stringify(req),      // body: req데이터를 JSON형태로 전달해야함
+    });
 }
